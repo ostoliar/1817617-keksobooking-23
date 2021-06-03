c641d7bd08ffaecc441f7acf389283e4ff4b53db
Oleksandr Stoliar <o.stoliar@viseven.com>
2021-06-03 14:40:00 +0300

    update(main.js): fix try bloks

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 function randomNumber(from, to) {
   let message = document.querySelector('.notice__title');
-  if (to <= from) {
     try { 
       if(to < from)  throw '«до» меньшее, чем значение «от»';
       if(to === from) throw '«до» равное значению «от».';
@@ -8,7 +7,6 @@ function randomNumber(from, to) {
     catch(err) {
       message.innerHTML = 'Переданное значение ' + err;
     }
-  }
   from = Math.ceil(from);
   to = Math.floor(to);
   return Math.floor(Math.random() * (to - from + 1)) + from;
@@ -18,7 +16,6 @@ randomNumber(9, 30);
 
 function randomCoordinates(from, to, quantityAfterDecimal) {
   let message = document.querySelector('.notice__title');
-  if (to <= from) {
     try { 
       if(to < from)  throw '«до» меньшее, чем значение «от»';
       if(to === from) throw '«до» равное значению «от».';
@@ -26,7 +23,6 @@ function randomCoordinates(from, to, quantityAfterDecimal) {
     catch(err) {
       message.innerHTML = 'Переданное значение ' + err;
     }
-  }
   from = Math.ceil(from);
   to = Math.floor(to);
   const number = Math.random() * (to - from + 1) + from;
