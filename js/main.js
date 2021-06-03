e6cb61df174ed96d2acb6009e2785f7e34f28a5b
Oleksandr Stoliar <o.stoliar@viseven.com>
2021-06-03 14:05:29 +0300

    update(main.js): fix functions

--- js/main.js
+++ js/main.js
@@ -1,28 +1,37 @@
 function randomNumber(from, to) {
+  let message = document.querySelector('.notice__title');
   if (to <= from) {
-    alert(
-      "Переданное значение «до» меньшее, чем значение «от», или равное ему."
-    );
+    try { 
+      if(to < from)  throw 'Переданное значение «до» меньшее, чем значение «от»';
+      if(to === from) throw 'Переданное значение «до» равное значению «от».';
+    }
+    catch(err) {
+      message.innerHTML = 'Переданное значение ' + err;
+    }
   }
   from = Math.ceil(from);
   to = Math.floor(to);
   return Math.floor(Math.random() * (to - from + 1)) + from;
 }
 
-console.log(randomNumber(9, 30));
+randomNumber(9, 30);
 
 function randomCoordinates(from, to, quantityAfterDecimal) {
-  if (to < from) {
-    alert("Переданное значение «до» меньшее, чем значение «от».");
-  }
-  if (to === from) {
-    alert("Переданное значение «до» равное значению «от».");
+  let message = document.querySelector('.notice__title');
+  if (to <= from) {
+    try { 
+      if(to < from)  throw 'Переданное значение «до» меньшее, чем значение «от»';
+      if(to === from) throw 'Переданное значение «до» равное значению «от».';
+    }
+    catch(err) {
+      message.innerHTML = 'Переданное значение ' + err;
+    }
   }
   from = Math.ceil(from);
   to = Math.floor(to);
-  let number = Math.random() * (to - from + 1) + from;
-  let randomCoordinate = number.toFixed(quantityAfterDecimal);
+  const number = Math.random() * (to - from + 1) + from;
+  const randomCoordinate = number.toFixed(quantityAfterDecimal);
   return randomCoordinate;
 }
 
-console.log(randomCoordinates(0, 9.8, 2));
+randomCoordinates(0, 9.8, 2);
