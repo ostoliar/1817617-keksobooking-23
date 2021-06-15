2f54b42585a17e061086afe8a23633990edd80c4
Oleksandr Stoliar <o.stoliar@viseven.com>
2021-06-15 22:16:01 +0300

    update(*): add map

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+/* map*/
+const myMap = L.map('mapid').setView([35.6895, 139.69171], 13);
+
+const tileLayer = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  maxZoom: 18,
+  id: 'mapbox/streets-v11',
+  tileSize: 512,
+  zoomOffset: -1,
+  accessToken: 'pk.eyJ1IjoiZXJ0ZWthIiwiYSI6ImNrcHgydmJrMjEyaDYybm56OHkzZWg4cjEifQ.DtvAiyAnZ6L54Jt7OCE7Dg',
+}).addTo(myMap);
+
+
+tileLayer.on('load',function() { console.log('all visible tiles have been loaded') });
+/* end map*/
 const author = {
   avatar: 'img/avatars/user',
 };
