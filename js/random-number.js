3090cfe1ac8c6d50d4db2b902297fdb207e8311f
Oleksandr Stoliar <o.stoliar@viseven.com>
2021-06-18 12:59:41 +0300

    update(random-number.js): fix after review

--- js/random-number.js
+++ js/random-number.js
@@ -4,52 +4,65 @@ function showErrorMessage(err) {
   message.innerHTML = `Переданное значение ${err}`;
 }
 
-// function getRandomNumber(from, to) {
-//   if (to < from) {
-//     throw new Error("«до» меньшее, чем значение «от»");
-//   }
-//   if (to === from) {
-//     throw new Error("«до» равное значению «от».");
-//   }
-//   const roundedFrom = Math.ceil(from);
-//   const roundedTo = Math.floor(to);
-
-//   return (
-//     Math.floor(Math.random() * (roundedTo - roundedFrom + 1)) + roundedFrom
-//   );
-// }
-
 function getRandomNumber(from, to) {
   if (to < from) {
-    showErrorMessage('«до» меньшее, чем значение «от»');
-    return -1;
+    return {
+      error: true,
+      msg: '«до» меньшее, чем значение «от»',
+    };
   }
   if (to === from) {
-    showErrorMessage('«до» равное значению «от».');
-    return -1;
+    return {
+      error: true,
+      msg: '«до» равное значению «от».',
+    };
   }
   const roundedFrom = Math.ceil(from);
   const roundedTo = Math.floor(to);
+  const number = Math.floor(Math.random() * (roundedTo - roundedFrom + 1)) + roundedFrom;
 
-  return (
-    Math.floor(Math.random() * (roundedTo - roundedFrom + 1)) + roundedFrom
-  );
+  return {
+    error: false,
+    data: number,
+  };
+}
+
+const result = getRandomNumber(2, 20);
+if(result.error){
+  showErrorMessage(result.msg);
+}else{
+  result.data;
 }
 
 function getRandomCoordinates(from, to, quantityAfterDecimal) {
   if (to < from) {
-    showErrorMessage('«до» меньшее, чем значение «от»');
-    return -1;
+    return {
+      error: true,
+      msg: '«до» меньшее, чем значение «от»',
+    };
   }
   if (to === from) {
-    showErrorMessage('«до» равное значению «от».');
-    return -1;
+    return {
+      error: true,
+      msg: '«до» равное значению «от».',
+    };
   }
   const roundedFrom = Math.ceil(from);
   const roundedTo = Math.floor(to);
   const randomNumber = Math.random() * (roundedTo - roundedFrom) + roundedFrom;
+  const coordinates =  randomNumber.toFixed(quantityAfterDecimal);
 
-  return randomNumber.toFixed(quantityAfterDecimal);
+  return {
+    error: false,
+    data: coordinates,
+  };
+}
+
+const resultCoordinates = getRandomCoordinates(4, 20);
+if(resultCoordinates.error){
+  showErrorMessage(resultCoordinates.msg);
+}else{
+  result.data;
 }
 
 export {getRandomNumber, getRandomCoordinates};
