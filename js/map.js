96b3202113f8672538f4e69c94e1d1bdf1a465c7
Oleksandr Stoliar <o.stoliar@viseven.com>
2021-06-24 17:24:18 +0300

    update(*): add map

---
+++ js/map.js
@@ -0,0 +1,18 @@
+const myMap = L.map('mapId').setView([35.6895, 139.69171], 13);
+
+const tileLayer = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  maxZoom: 18,
+  id: 'mapbox/streets-v11',
+  tileSize: 512,
+  zoomOffset: -1,
+  accessToken: 'pk.eyJ1IjoiZXJ0ZWthIiwiYSI6ImNrcHgydmJrMjEyaDYybm56OHkzZWg4cjEifQ.DtvAiyAnZ6L54Jt7OCE7Dg',
+}).addTo(myMap);
+
+const form = document.querySelector('.ad-form');
+
+
+tileLayer.on('load',() => { form.style.pointerEvents = 'none'; });
+
+
+export {myMap, tileLayer};
