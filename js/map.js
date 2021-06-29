ca5ffb99bb200935222a2dc6de9b080a1c908b42
Oleksandr Stoliar <o.stoliar@viseven.com>
2021-06-29 12:33:28 +0300

    update(map.js): fix after review

--- js/map.js
+++ js/map.js
@@ -3,16 +3,19 @@ import { form } from './offer-template.js';
 const webToken = 'pk.eyJ1IjoiZXJ0ZWthIiwiYSI6ImNrcHgydmJrMjEyaDYybm56OHkzZWg4cjEifQ.DtvAiyAnZ6L54Jt7OCE7Dg';
 const latitude = 35.6895;
 const longitude = 139.69171;
-const myMap = L.map('mapId').setView([latitude, longitude], 13);
 
-const tileLayer = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// L - its var of leaflet library
+const myMap = L.map('mapId').setView([latitude, longitude], 13);
+const leafletOptions = {
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
   maxZoom: 18,
   id: 'mapbox/streets-v11',
   tileSize: 512,
   zoomOffset: -1,
   accessToken: webToken,
-}).addTo(myMap);
+};
+
+const tileLayer = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', leafletOptions).addTo(myMap);
 
 const formFilters = document.querySelector('.map__filters');
 
