f37cbc867834951ef3413a5c3d379f22e225d410
Oleksandr Stoliar <o.stoliar@viseven.com>
2021-07-11 16:37:30 +0300

    update(main.js): fix after review

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 import {similarOffer} from './create-offer.js';
 import {getOfferTemplate, template} from './offer-template.js';
-import {tileLayer, myMap} from './map.js';
+import {tileLayer, myMap, setMapMarker} from './map.js';
 import {priceArea, formTitle} from './form-validation.js';
-import {offer} from './data.js';
+import { getOffers } from './data.js';
+import {FILE_TYPES, fileChooser, preview, previewApartmentPhotos, fileChooserApartmentPhotos} from './avatar.js';
 
 similarOffer;
 template;
@@ -10,7 +11,20 @@ tileLayer;
 myMap;
 priceArea;
 formTitle;
-getOfferTemplate;
-offer;
+preview;
+fileChooser;
+FILE_TYPES;
+fileChooserApartmentPhotos;
+previewApartmentPhotos;
+
+async function loadOffers() {
+  const offers = await getOffers();
+  offers.forEach((item) => {
+    const offerTemplate = getOfferTemplate(item.offer);
+    setMapMarker(item.location, offerTemplate);
+  });
+}
+
+loadOffers();
 
 
